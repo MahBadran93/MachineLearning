04917aeddc9b0e5217c1eaa848b603d73a8c044b
Mahmoud <mahmoud.s.badran@gail.com>
2020-03-18 14:43:29 +0100

    edit some commints

--- Project1/Project1EditedFinal/Funcs.py
+++ Project1/Project1EditedFinal/Funcs.py
@@ -4,49 +4,49 @@ def update(weightData, trainingData, learningRate):
     # weightData   : parameter to pass the weightData winner vector for weighData array, weightData[winner].
     # trainingData : parameter to pass the trainingData[index], index of trainingData we are testing currently.
     # learningRate : pass the learingRate.
-    
+
+    # this function returns an updated weight data for winner part
     updatedValues =  weightData + learningRate*(trainingData - weightData)
-    return updatedValues # it returns an updated weight data for winner part
+    return updatedValues 
 #==========================================================================================================
 
 def EcuDist(trainDataVector, weightDataVector):
     # trainDataVector: pass the traing data vector.
     # weightDataVector: pass the weight data vector 
+
+     # tbis function returns the Ecu. distance between two passed vectors
     resultSum = np.sum((trainDataVector-weightDataVector)**2)
     return resultSum    
-    # it returns the Ecu. distance between two passed vectors.
+   
 #==========================================================================================================
-"""
-you pass this function the weigted matrix(output nodes) and the vector form input matrix that 
-you want to check distance with the weighted matrix
-"""
-def min(weightData , trainingDataVector ):
+
+def min(weightData , dataSetVector ):
     # weightData: pass the whole weightData array.
-    # trainingDataVector: pass the trainingDataVector we are currently testing.
-    # index : flag for holding the winner index vector.
-    # weightDataTestVectorNum: parameter to have the number of rows in the weightData array to test with each training data vector.
-    index = 0
-    minValue = EcuDist(trainingDataVector,weightData[0]) # initalize the min
-    #minValue = 0
+    # dataSetVector: pass the a data vector to compare with weightdata array, to find its closest match 
+    """
+     this function returns the index of winner in the weigted matrix(closest match with trainingdata vector). result is 0 ro 1
+    """
+    winnerIndex = 0 #flag for initalizing the winner index
+    minValue = EcuDist(dataSetVector,weightData[0]) # initalize the minValue
+    # iterate through all weighdata rows to find the closest match, depending on ecu. distance,
+    #and then return the index of the closest match(winner)
     for i in range(weightData.shape[0]):
-        if(EcuDist(trainingDataVector,weightData[i]) < minValue):
-            minValue = EcuDist(trainingDataVector,weightData[i])
-            index = i
-    return index 
+        if(EcuDist(dataSetVector,weightData[i]) < minValue):
+            minValue = EcuDist(dataSetVector,weightData[i])
+            winnerIndex = i
+    return winnerIndex 
      
-    # return the index of winner vector in the weigted matrix nural,
-    # nuralsDataArray, return either 0 or 1, class I, or class II
+   
 
 #==================================================================================
 def trainData(weightData, trainData, iteration, learningRate):
-    print(trainData.shape[0])
     for i in range(iteration):
         for subj in range (trainData.shape[0]):
-            # trainData.shape[0] : number of training data i have
-            winner = min(weightData, trainData[subj])
-            updatedVector = update(weightData[winner], trainData[subj], learningRate)
-            weightData[winner] = updatedVector
-        learningRate = learningRate * 0.5
-    return weightData # return the trained weight     
+            # trainData.shape[0] : number of training data(subjects) I have
+            winner = min(weightData, trainData[subj]) #return index of winner to update accordingly
+            updatedVector = update(weightData[winner], trainData[subj], learningRate) # return updated values
+            weightData[winner] = updatedVector #replace the winner from  the weightdata with updated one
+        learningRate = learningRate * 0.5 #change learning Rate each iteration 
+    return weightData # return the final trained weight     
 
 #==================================================================================
